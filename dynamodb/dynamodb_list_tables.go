d710e78fa8496d2ee14d0c79e1c2320663cf7110
Prashanth Rao <pbrao@pmbrao.com>
2019-01-24 22:20:22 -0600

    Initial checkin

---
+++ dynamodb/dynamodb_list_tables.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+func main() {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	})
+	svc := dynamodb.New(sess)
+
+	result, err := svc.ListTables(&dynamodb.ListTablesInput{})
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Tables:")
+	fmt.Println("")
+
+	for _, n := range result.TableNames {
+		fmt.Println(*n)
+	}
+}
